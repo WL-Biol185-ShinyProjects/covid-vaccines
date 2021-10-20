68978b3c08f33f75c7128ef6ba0f0c4ea8d7789c
Katie Kern <kernk22@mail.wlu.edu>
2021-10-20 15:56:03 -0400

    sourcing out the 3d globe

---
+++ pfizer3dglobe.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(threejs)
+library("maptools")
+library("maps")
+library("writexl")
+
+worldcities_pfizer <- read_csv("CSVs/worldcities_pfizer.csv")
+data(worldcities_pfizer, package="maps")
+cities <- worldcities_pfizer[order(worldcities_pfizer$vaccines_by_manu,decreasing=TRUE)[1:37],]
+value  <- 100 * cities$vaccines_by_manu / max(cities$vaccines_by_manu)
+
+globejs(bg="black", lat=cities$lat,     long=cities$long, value=value, 
+        rotationlat=-0.34,     rotationlong=-0.38, fov=30)
