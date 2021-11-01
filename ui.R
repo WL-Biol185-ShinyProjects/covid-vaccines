cd3db62292b9d4a2e1fb004182e9ddbe185a4850
Jack Donahue <donahuej22@mail.wlu.edu>
2021-11-01 14:51:07 -0400

    gresat

--- ui.R
+++ ui.R
@@ -17,16 +17,18 @@ dashboardPage(skin = "blue",
   
 
   dashboardSidebar(
+    
+    a(href = "https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html"),  
  
     sidebarMenu(
       
-      dateRangeInput("daterange3", "Date range:",
-                     start  = "2020-08-01",
-                     end    = "2021-9-31",
-                     min    = "2020-08-01",
-                     max    = "2021-9-31",
-                     format = "mm/dd/yy",
-                     separator = " - "),
+      # dateRangeInput("daterange3", "Date range:",
+      #                start  = "2020-08-01",
+      #                end    = "2021-9-31",
+      #                min    = "2020-08-01",
+      #                max    = "2021-9-31",
+      #                format = "mm/dd/yy",
+      #                separator = " - "),
       
       menuItem("Global View", tabName = "Global View", 
                icon = icon("globe", lib = "glyphicon")),
@@ -39,44 +41,70 @@ dashboardPage(skin = "blue",
       
       menuItem("Continents", tabName = "Continents", 
                icon = icon("th"))
+      # menuItem("CDC Vaccine Info", icon = icon("file-code-o"),
+      #          href = "https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html")
     )
   ),
+  
+  
   dashboardBody(
     
-    tags$head( 
-      tags$style(HTML('.main-sidebar {
-        font-family: "Lucida Console", monospace;
-        font-weight: bold;
-        font-size: 14px; }'))
+    tabItems(
+      
+      tabItem(tabName = "Global View",
+              
+              ),
+      
+      tabItem(tabName = "Variants by Country",
+              
+              fluidRow(
+              
+                box(globeOutput("pfizer_globe")))
+              ),
+      
+      tabItem(tabName = "Vaccine and Wealth"),
+      
+      tabItem(tabName = "Continents")
     ),
     
-    fluidRow(
-      
-      valueBoxOutput("CountryBox"),
-      
-      valueBoxOutput("VaccinatedBox"),
-      
-      valueBoxOutput("Predominant_VaccineBox")
-    ),
+
     
-    fluidRow(
-      
-      box(width = 4, actionButton("count", "Increment progress"))
-    ),
+    # tags$head( 
+    #   tags$style(HTML('.main-sidebar {
+    #     font-family: "Lucida Console", monospace;
+    #     font-weight: bold;
+    #     font-size: 14px; }'))
+    # ),
     
-    fluidRow(
-       box(width = 12, globeOutput("pfizer_globe"))
-    ),
+
+    
+    # fluidRow(
+    # 
+    #   valueBoxOutput("CountryBox"),
+    # 
+    #   valueBoxOutput("VaccinatedBox"),
+    # 
+    #   valueBoxOutput("Predominant_VaccineBox")
+    # ),
+    # 
+    # fluidRow(
+    #   
+    #   box(width = 4, actionButton("count", "Increment progress"))
+    # ),
+    # 
+    # fluidRow(
+    #    box(width = 12, globeOutput("pfizer_globe"))
+    # ),
     
     uiOutput("hearbeat")
     ),
-  
+
     
-  tags$head(tags$style(HTML('
-      .main-header .logo {
-        font-family: "Lucida Console", monospace;
-        font-weight: bold;
-        font-size: 24px;
-      }'
-  )))
+  # tags$head(tags$style(HTML('
+  #     .main-header .logo {
+  #       font-family: "Lucida Console", monospace;
+  #       font-weight: bold;
+  #       font-size: 24px;
+  #     }'
+  # )))
   )
