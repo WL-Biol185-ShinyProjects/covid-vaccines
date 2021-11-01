ec3a072619132f25d657a556f09f30ba7f17c456
Jack Donahue <donahuej22@mail.wlu.edu>
2021-11-01 15:46:14 -0400

    yep

--- server.R
+++ server.R
@@ -23,7 +23,7 @@ function(input, output, session) {
   output$pfizer_globe <- renderGlobe({
     
     worldcities_pfizer <- read_csv("CSVs/worldcities_pfizer.csv")
-    data(worldcities_pfizer, package="maps")
+    # data(worldcities_pfizer, package="maps")
     cities <- worldcities_pfizer[order(worldcities_pfizer$vaccines_by_manu,decreasing=TRUE)[1:37],]
     value  <- 1000 * cities$vaccines_by_manu / max(cities$vaccines_by_manu)
     pfizer_globe <-globejs(bg="black", lat=cities$lat,     long=cities$long, value=value, 
