dd6ea2492e6dbe307caee73ed3bed39eecd3145d
Emma Aldrich <aldriche22@mail.wlu.edu>
2021-11-01 15:20:25 -0400

    added filter for globe

--- server.R
+++ server.R
@@ -10,23 +10,40 @@ library("writexl")
 
 function(input, output) {
 
-  
+
   output$summary <- renderText({
     summary(x())
   })
-  
+
+
+
 }
 
 
 function(input, output, session) {
   
+  
+  x<- reactive({
+    if(input$vaccine == "Pfizer") {
+      worldcities_all_manu <- worldcities_all_manu %>%
+        filter(vaccine == "Pfizer/BioNTech")}
+    else if(input$vaccine == "Moderna") {
+      worldcities_all_manu <- worldcities_all_manu %>%
+        filter(vaccine == "Moderna")}
+    else if(input$vaccine == "AstraZeneca") {
+      worldcities_all_manu <- worldcities_all_manu %>%
+        filter(vaccine == "Oxford/AstraZeneca")}
+    else if(input$vaccine == "Johnson&Johnson") {
+      worldcities_all_manu <- worldcities_all_manu %>%
+        filter(vaccine == "JJ")}
+  })
+  
   output$pfizer_globe <- renderGlobe({
     
-    worldcities_pfizer <- read_csv("CSVs/worldcities_pfizer.csv")
-    data(worldcities_pfizer, package="maps")
-    cities <- worldcities_pfizer[order(worldcities_pfizer$vaccines_by_manu,decreasing=TRUE)[1:37],]
+    data(x, package="maps")
+    cities <- x[order(x$vaccines_by_manu,decreasing=TRUE)[1:37],]
     value  <- 1000 * cities$vaccines_by_manu / max(cities$vaccines_by_manu)
-    pfizer_globe <-globejs(bg="black", lat=cities$lat,     long=cities$long, value=value, 
+    manu_globe <-globejs(bg="black", lat=cities$lat,     long=cities$long, value=value, 
                            rotationlat=-0.34,     rotationlong=-0.38, fov=30)
   })
   
@@ -64,4 +81,5 @@ function(input, output, session) {
   })
   
 }
+
 
