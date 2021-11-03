5ff638cd222a2f9b5de4ff0ff8e4d980ee5b04ee
Emma Aldrich <aldriche22@mail.wlu.edu>
2021-11-03 16:03:42 -0400

    mutated hopkins so that we can plot GDP per capita

--- For Website Script Files/socioeconomic.R
+++ For Website Script Files/socioeconomic.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 time_series_covid19_vaccine_global <- read_csv("https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv")
 hopkins <- time_series_covid19_vaccine_global
 
@@ -10,4 +12,12 @@ hopkins <- hopkins %>%
 
 GDP_per_capita <- read.csv("~/covid-vaccines/CSVs/GDP_per_capita.csv")
 
-hopkins_GDP <- left_join(hopkins, GDP_per_capita, by = c("Country_Region" = "Country"))
+hopkins_GDP <- left_join(hopkins, GDP_per_capita, by = c("Country_Region" = "Country")) %>%
+  mutate(GDP.nominal.per.capita = gsub("$", "", GDP.nominal.per.capita, fixed = TRUE)) %>%
+  mutate(GDP.nominal.per.capita = gsub(",", "", GDP.nominal.per.capita, fixed = TRUE)) %>% 
+  mutate(GDP.nominal.per.capita = as.numeric(GDP.nominal.per.capita))
+
+
+
+
+
