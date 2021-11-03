19805a283cd5399e1050c64d4175367809d2a5f6
Emma Aldrich <aldriche22@mail.wlu.edu>
2021-11-03 15:53:00 -0400

    made a new .R for socioeconomic data (comparing GDP per capita with hopkins vaccination data)
    made HOpkins into a csv (doesn't read well, use online version)
    
    made a CSV with GDP per captia

---
+++ For Website Script Files/socioeconomic.R
@@ -0,0 +1,13 @@
+time_series_covid19_vaccine_global <- read_csv("https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv")
+hopkins <- time_series_covid19_vaccine_global
+
+hopkins <- hopkins %>%
+  filter(Date == "2021-10-25") %>%
+  filter(!is.na(People_fully_vaccinated)) %>%
+  filter(Country_Region != "US (Aggregate)") %>%
+  filter(Country_Region != "World")
+  
+
+GDP_per_capita <- read.csv("~/covid-vaccines/CSVs/GDP_per_capita.csv")
+
+hopkins_GDP <- left_join(hopkins, GDP_per_capita, by = c("Country_Region" = "Country"))
