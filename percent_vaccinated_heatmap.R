4368012508bd7c31054b2e67f7ce9d97358a2fe0
Emma Aldrich <aldriche22@mail.wlu.edu>
2021-11-17 16:01:33 -0500

    added a plot for partially (not including fully) vaxxed

--- percent_vaccinated_heatmap.R
+++ percent_vaccinated_heatmap.R
@@ -38,17 +38,21 @@ hopkins_pop$percent_fully <- (hopkins_pop$People_fully_vaccinated / hopkins_pop$
 
 world_geo <- readOGR("/home/gregg/countries.geo.json")
 world_geo_vax <- read.csv("~/covid-vaccines/CSVs/world_geo_vax1.csv") 
+world_geo_vax$people_partial_not_full <- world_geo_vax$People_partially_vaccinated - world_geo_vax$People_fully_vaccinated 
+world_geo_vax$percent_partial_not_full <- 100*(world_geo_vax$people_partial_not_full / world_geo_vax$Population..2020.)
 
 
 world_geo@data <- left_join(world_geo@data, world_geo_vax, by = "name", "name")
 
 #make the map 
-mybins <- c(0,15,30,45,60,75,90,100)
+mybins <- c(0,5,10,15,20,25,30,35)
 mypalette <- colorBin( palette="YlOrBr", domain=world_geo@data, na.color="transparent", bins=mybins)
 
 #fix precision 
 world_geo$percent_partial <- round(world_geo$percent_partial, digits = 2)
 world_geo$People_fully <- round(world_geo$People_fully, digits = 2)
+world_geo$percent_partial_not_full.y <- round(world_geo$percent_partial_not_full.y, digits = 2)
+
 partial_popup <- paste0("<strong>Country: </strong>", 
                         world_geo$name,
                         "<br><strong>Partially Vaccinated: </strong>", 
@@ -62,7 +66,16 @@ full_popup <- paste0("<strong>Country: </strong>",
                         "<br><strong>Percent of Population Fully Vaccinated </strong>",
                      world_geo$People_fully)
 
+
+partial_not_full_popup <- paste0("<strong>Country: </strong>", 
+                                 world_geo$name,
+                                 "<br><strong>Partially Vaccinated: </strong>", 
+                                 world_geo$people_partial_not_full.x, 
+                                 "<br><strong>Percent of Population Partially Vaccinated (not including fully) </strong>",
+                                 world_geo$percent_partial_not_full.y)
+
 #make the maps!
+#partial 
 leaflet(world_geo) %>% 
   addTiles()  %>% 
   setView( lat=10, lng=0 , zoom=2) %>%
@@ -81,23 +94,43 @@ leaflet(world_geo) %>%
   ) %>%
   addLegend( pal=mypalette, values=~percent_partial, opacity=0.9, title = "Percent partially vaccinated by country", position = "bottomleft" ) 
 
+#partial but not including fully 
+leaflet(world_geo) %>% 
+  addTiles()  %>% 
+  setView( lat=10, lng=0 , zoom=2) %>%
+  addPolygons( 
+    popup = partial_not_full_popup,
+    fillColor = ~mypalette(world_geo$percent_partial_not_full.y), 
+    stroke=TRUE, 
+    fillOpacity = 0.9, 
+    color="white",
+    weight=0.3,
+    labelOptions = labelOptions( 
+      style = list("font-weight" = "normal", padding = "3px 8px"), 
+      textsize = "13px", 
+      direction = "auto"
+    )
+  ) %>%
+  addLegend( pal=mypalette, values=~percent_partial_not_full.y, opacity=0.9, title = "Percent partially (but not fully) vaccinated by country", position = "bottomleft" ) 
 
-# leaflet(world_geo) %>% 
-#   addTiles()  %>% 
-#   setView( lat=10, lng=0 , zoom=2) %>%
-#   addPolygons(
-#     popup = full_popup,
-#     fillColor = ~mypalette(world_geo$People_fully), 
-#     stroke=TRUE, 
-#     fillOpacity = 0.9, 
-#     color="white",
-#     weight=0.3,
-#     labelOptions = labelOptions( 
-#       style = list("font-weight" = "normal", padding = "3px 8px"), 
-#       textsize = "13px", 
-#       direction = "auto"
-#     )
-#   ) %>%
-#   addLegend( pal=mypalette, values=~People_fully, opacity=0.9, title = "Percent fully vaccinated by country", position = "bottomleft" ) 
+
+#full 
+leaflet(world_geo) %>%
+  addTiles()  %>%
+  setView( lat=10, lng=0 , zoom=2) %>%
+  addPolygons(
+    popup = full_popup,
+    fillColor = ~mypalette(world_geo$People_fully),
+    stroke=TRUE,
+    fillOpacity = 0.9,
+    color="white",
+    weight=0.3,
+    labelOptions = labelOptions(
+      style = list("font-weight" = "normal", padding = "3px 8px"),
+      textsize = "13px",
+      direction = "auto"
+    )
+  ) %>%
+  addLegend( pal=mypalette, values=~People_fully, opacity=0.9, title = "Percent fully vaccinated by country", position = "bottomleft" )
 
 
