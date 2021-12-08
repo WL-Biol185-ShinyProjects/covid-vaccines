76f77705ffdaeee0cad234249859af6ce5978777
Jack Donahue <donahuej22@mail.wlu.edu>
2021-12-08 16:15:13 -0500

    cleaned up format

--- server.R
+++ server.R
@@ -24,23 +24,33 @@ function(input, output, session) {
     worldcities_all_manu <- read_csv("CSVs/cat.csv")
     
     data(worldcities_all_manu, package = "maps") #injection scope, never ever do this again
-    x <- worldcities_all_manu %>% 
+    
+      x <- worldcities_all_manu %>% 
          filter(vaccine == input$vaccine) 
-    print(x)
-    nrows_x <- nrow(x)
-    cities <- x[order(x$vaccines_by_manu,decreasing=TRUE)[1:nrows_x],]
-    value  <- 1000 * cities$vaccines_by_manu / max(cities$vaccines_by_manu)
-    manu_globe <-globejs(bg="black", lat=cities$lat,     long=cities$long, value=value, 
-                           rotationlat=-0.34,     rotationlong=-0.38, fov=30)
-  })
+      
+      nrows_x <- nrow(x)
+      
+      cities <- x[order(x$vaccines_by_manu,decreasing=TRUE)[1:nrows_x],]
+      
+      value  <- 1000 * cities$vaccines_by_manu / max(cities$vaccines_by_manu)
+      
+      manu_globe <-globejs(bg = "black", 
+                          lat = cities$lat,     
+                         long = cities$long, 
+                        value = value, 
+                  rotationlat = -0.34,     
+                 rotationlong = -0.38, 
+                          fov = 30)
+                             })
   
   
   output$percapita <- renderPlot({ 
     
-    time_series_covid19_vaccine_global <- read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+    time_series_covid19_vaccine_global <- 
+      read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+    
     hopkins <- time_series_covid19_vaccine_global
     
-    #left join hopkins with world cities in order to get population to do fully vaccinated per capita
     country_pop <- read.csv("CSVs/population_by_country_2020.csv") %>%
       mutate(Country..or.dependency. = as.character(Country..or.dependency.)) %>%
       mutate(pop = Population..2020.)
@@ -57,20 +67,25 @@ function(input, output, session) {
       arrange(desc(People_fully_vaccinated)) %>%
       slice(1:10)
     
-    pop_hopkins_fully <- left_join(hopkins_fully, country_pop, by = c("Country_Region" = "Country..or.dependency.")) %>%
+    pop_hopkins_fully <- left_join(hopkins_fully,
+                                   country_pop, 
+                                   by = c("Country_Region" = "Country..or.dependency.")
+                                   ) %>%
       mutate(fully_per_capita = People_fully_vaccinated / pop) %>%
       mutate(World.Share = gsub("%", "", World.Share, fixed = TRUE)) %>%
       mutate(World.Share = as.numeric(World.Share))
     
-    ggplot(pop_hopkins_fully, aes(reorder(Country_Region, fully_per_capita), 
-
-                                  fully_per_capita, fill = World.Share)) + 
-      geom_bar(stat = "identity") +
-      theme_classic() +
-      labs(title = "Fully Vaccinated Rates Per Capita (Countries with Highest Number of Vaccines Distribued)", 
-           subtitle = "Colored by % of World Population for Each Country", 
-           y = "Fully Vaccinated Individuals per Capita",
-           x = "Country")
+    ggplot(pop_hopkins_fully, 
+           aes(reorder(Country_Region,
+                       fully_per_capita),
+                       fully_per_capita,
+                       fill = World.Share)) + 
+          geom_bar(stat = "identity") +
+          theme_classic() +
+          labs(title = "Fully Vaccinated Rates Per Capita (Countries with Highest Number of Vaccines Distribued)", 
+            subtitle = "Colored by % of World Population for Each Country", 
+                   y = "Fully Vaccinated Individuals per Capita",
+                   x = "Country")
     
     })
   
@@ -114,19 +129,22 @@ function(input, output, session) {
       facet_wrap(~year) +
       coord_flip() +
       theme_classic() +
-      labs(title = "Covid-19 Variant Prevelance By Year", y = "Relevant Prevelance", x = "Month", fill = "Variant")
-    
-    
-    
-    })
+      labs(title = "Covid-19 Variant Prevelance By Year", 
+               y = "Relevant Prevelance", 
+               x = "Month", 
+            fill = "Variant")
+                                      })
   
   output$distPlot <- renderPlot({
+    
     hist(rnorm(input$obs))
-  })
+                                })
   
   output$VaccinatedBox <- renderValueBox({
     
-    time_series_covid19_vaccine_global <- read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+    time_series_covid19_vaccine_global <- 
+      read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+    
     hopkins <- time_series_covid19_vaccine_global %>%
       filter(Date == "2021-10-25") 
   
@@ -134,16 +152,16 @@ function(input, output, session) {
       filter(Country_Region == input$Country) %>%
       filter(!(is.na(People_fully_vaccinated)))
     value <- HopkinsVAX[1,5]
-    print("Hello")
-    print(value)
     valueBox(
       paste0(value), "Individuals Fully Vaccinated", 
-      icon = icon("heart", lib = "glyphicon"),
-      color = "green")
+      icon = icon("heart", 
+       lib = "glyphicon"),
+     color = "green")
     
-  })
+                                       })
   
   output$Predominant_VaccineBox <- renderValueBox({
+    
     pop_hop_fully <- read.csv("CSVs/GDP_population_vaccine.csv")
     pop_hop_fully <- pop_hop_fully %>%
       filter(Country_Region == input$Country) %>%
@@ -154,36 +172,41 @@ function(input, output, session) {
     value <- pop_hop_fully[1,13]
     valueBox(
       paste0(value), "GDP Per Capita",
-      icon = icon("briefcase", lib = "glyphicon"),
-      color = "teal",
-    )
-  })
+      icon = icon("briefcase", 
+       lib = "glyphicon"),
+     color = "teal")
+    
+                                                })
   
   output$CountryBox <- renderValueBox({
+    
     pop_hop_fully <- read.csv("CSVs/GDP_population_vaccine.csv")
     pop_hop_fully <- pop_hop_fully %>%
       filter(Country_Region == input$Country) %>%
       filter(!(is.na(People_partially_vaccinated)))
     
-    
-    
     value <- pop_hop_fully[1,1]
     
     valueBox(
       paste0(value), "Selected Country", 
-      icon = icon("search", lib = "glyphicon"),
-      color = "olive",
-    )
-  })
+      icon = icon("search", 
+       lib = "glyphicon"),
+     color = "olive")
+    
+                                     })
   
   output$heartbeat <- renderUI({
+    
     invalidateLater(1000, session)
     p(Sys.time(),style = "visibility: hidden;")
-  })
+    
+                               })
   
   output$socioeconomicsbox <- renderPlot({
 
-    time_series_covid19_vaccine_global <- read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+    time_series_covid19_vaccine_global <- 
+      read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+    
     hopkins <- time_series_covid19_vaccine_global
     GDP_per_capita <- read.csv("CSVs/GDP_per_capita.csv")
 
@@ -193,7 +216,9 @@ function(input, output, session) {
       filter(Country_Region != "US (Aggregate)") %>%
       filter(Country_Region != "World")
 
-    hopkins_GDP <- left_join(hopkins, GDP_per_capita, by = c("Country_Region" = "Country"))
+    hopkins_GDP <- left_join(hopkins, 
+                             GDP_per_capita, 
+                             by = c("Country_Region" = "Country"))
 
     hopkins_GDP <- hopkins_GDP %>%
       mutate(GDP.nominal.per.capita = gsub("$", "", GDP.nominal.per.capita, fixed = TRUE)) %>%
@@ -228,16 +253,26 @@ function(input, output, session) {
     pop_hopkins_fully$fully_per_capita <- as.factor(pop_hopkins_fully$fully_per_capita)
 
 
-    ggplot(data = pop_hopkins_fully, aes(x = fully_per_capita, y = Country_Region, fill = GDP.nominal.per.capita)) +
+    ggplot(data = pop_hopkins_fully, aes(x = fully_per_capita, 
+                                         y = Country_Region, 
+                                      fill = GDP.nominal.per.capita)) +
       geom_tile()+
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-      labs(title = "GDP and Vaccination Rates In the World", subtitle = "Top 10 and Bottom 10 GDP countries chosen", x = "Percent Fully Vaccinated Per Country", y = "Country", fill = "Nominal GDP Per Capita")
+      theme(axis.text.x = element_text(angle = 90, 
+                                       vjust = 0.5, 
+                                       hjust = 1)) +
+      labs(title = "GDP and Vaccination Rates In the World", 
+           subtitle = "Top 10 and Bottom 10 GDP countries chosen",
+                  x = "Percent Fully Vaccinated Per Country", 
+                  y = "Country", 
+               fill = "Nominal GDP Per Capita")
 
-  })
+                                           })
   
   output$PercentVaccinatedHeatMap <- renderLeaflet({
     
-    time_series_covid19_vaccine_global <- read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+    time_series_covid19_vaccine_global <- 
+      read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+    
     hopkins <- time_series_covid19_vaccine_global
     
     hopkins <- hopkins %>%
@@ -252,30 +287,43 @@ function(input, output, session) {
     hopkins$Country_Region[143] <- "Taiwan"
     
 
-    hopkins_pop <- left_join(hopkins, population_by_country, by = c("Country_Region" = "Country..or.dependency.")) 
+    hopkins_pop <- left_join(hopkins, 
+                             population_by_country, 
+                             by = c("Country_Region" = "Country..or.dependency.")) 
     
 
-    hopkins_pop$percent_partial <- (hopkins_pop$People_partially_vaccinated / hopkins_pop$Population..2020.)*100
-    hopkins_pop$percent_fully <- (hopkins_pop$People_fully_vaccinated / hopkins_pop$Population..2020.)*100 
+    hopkins_pop$percent_partial <- 
+      (hopkins_pop$People_partially_vaccinated / hopkins_pop$Population..2020.)*100
+    
+    hopkins_pop$percent_fully <- 
+      (hopkins_pop$People_fully_vaccinated / hopkins_pop$Population..2020.)*100 
     
     
     world_geo <- readOGR("/home/gregg/countries.geo.json")
+    
     world_geo_vax <- read.csv("CSVs/world_geo_vax1.csv") 
     
-    
-    world_geo@data <- left_join(world_geo@data, world_geo_vax, by = "name", "name")
+    world_geo@data <- left_join(world_geo@data, 
+                                world_geo_vax, 
+                                by = "name", "name")
 
     mybins <- c(0,15,30,45,60,75,90,100)
-    mypalette <- colorBin( palette="YlOrBr", domain=world_geo@data, na.color="transparent", bins=mybins)
+    mypalette <- colorBin( palette = "YlOrBr", 
+                            domain = world_geo@data, 
+                          na.color = "transparent", 
+                              bins = mybins)
 
     world_geo$percent_partial <- round(world_geo$percent_partial, digits = 2)
+    
     world_geo$People_fully <- round(world_geo$People_fully, digits = 2)
+    
     partial_popup <- paste0("<strong>Country: </strong>", 
                             world_geo$name,
                             "<br><strong>Partially Vaccinated: </strong>", 
                             world_geo$People_partially_vaccinated, 
                             "<br><strong>Percent of Population Partially Vaccinated </strong>", 
                             world_geo$percent_partial)
+    
     full_popup <- paste0("<strong>Country: </strong>", 
                          world_geo$name,
                          "<br><strong>Fully Vaccinated: </strong>", 
@@ -285,31 +333,34 @@ function(input, output, session) {
     
     leaflet(world_geo) %>%
       addTiles()  %>%
-      setView( lat=10, lng=0 , zoom=2) %>%
-      addPolygons(
-        popup = full_popup,
-        fillColor = ~mypalette(world_geo$People_fully),
-        stroke=TRUE,
-        fillOpacity = 0.9,
-        color="white",
-        weight=0.3,
-        labelOptions = labelOptions(
-          style = list("font-weight" = "normal", padding = "3px 8px"),
-          textsize = "13px",
-          direction = "auto"
-        )
-      ) %>%
-      addLegend( pal=mypalette, values=~People_fully, opacity=0.9, title = "Percent fully vaccinated by country", position = "bottomleft" )
+      setView( lat = 10, 
+               lng = 0 , 
+              zoom = 2) %>%
+      addPolygons(popup = full_popup,
+              fillColor = ~mypalette(world_geo$People_fully),
+                 stroke = TRUE,
+            fillOpacity = 0.9,
+                  color = "white",
+                 weight = 0.3,
+           labelOptions = labelOptions(
+                  style = list("font-weight" = "normal", 
+                                     padding = "3px 8px"),
+                                    textsize = "13px",
+                                   direction = "auto")) %>%
+      addLegend(     pal = mypalette, 
+                  values =~ People_fully, 
+                 opacity = 0.9, 
+                   title = "Percent fully vaccinated by country", 
+                position = "bottomleft" )
     
-    
-    
-
-    
-  })
+                                                      })
+  
   
   output$PartiallyVaccinatedHeatmap <-  renderLeaflet({
       
-      time_series_covid19_vaccine_global <- read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+      time_series_covid19_vaccine_global <- 
+        read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+      
       hopkins <- time_series_covid19_vaccine_global
       
       hopkins <- hopkins %>%
@@ -324,30 +375,43 @@ function(input, output, session) {
       hopkins$Country_Region[143] <- "Taiwan"
       
       
-      hopkins_pop <- left_join(hopkins, population_by_country, by = c("Country_Region" = "Country..or.dependency.")) 
+      hopkins_pop <- left_join(hopkins, 
+                               population_by_country,
+                               by = c("Country_Region" = "Country..or.dependency.")) 
       
       
-      hopkins_pop$percent_partial <- (hopkins_pop$People_partially_vaccinated / hopkins_pop$Population..2020.)*100
-      hopkins_pop$percent_fully <- (hopkins_pop$People_fully_vaccinated / hopkins_pop$Population..2020.)*100 
+      hopkins_pop$percent_partial <- 
+        (hopkins_pop$People_partially_vaccinated / hopkins_pop$Population..2020.)*100
+      
+      hopkins_pop$percent_fully <- 
+        (hopkins_pop$People_fully_vaccinated / hopkins_pop$Population..2020.)*100 
       
       
       world_geo <- readOGR("/home/gregg/countries.geo.json")
+      
       world_geo_vax <- read.csv("CSVs/world_geo_vax1.csv") 
       
-      
-      world_geo@data <- left_join(world_geo@data, world_geo_vax, by = "name", "name")
+      world_geo@data <- left_join(world_geo@data, 
+                                  world_geo_vax, 
+                                  by = "name", "name")
       
       mybins <- c(0,15,30,45,60,75,90,100)
-      mypalette <- colorBin( palette="YlOrBr", domain=world_geo@data, na.color="transparent", bins=mybins)
+      mypalette <- colorBin( palette = "YlOrBr", 
+                              domain = world_geo@data, 
+                            na.color = "transparent", 
+                                bins = mybins)
       
       world_geo$percent_partial <- round(world_geo$percent_partial, digits = 2)
+      
       world_geo$People_fully <- round(world_geo$People_fully, digits = 2)
+      
       partial_popup <- paste0("<strong>Country: </strong>", 
                               world_geo$name,
                               "<br><strong>Partially Vaccinated: </strong>", 
                               world_geo$People_partially_vaccinated, 
                               "<br><strong>Percent of Population Partially Vaccinated </strong>", 
                               world_geo$percent_partial)
+      
       full_popup <- paste0("<strong>Country: </strong>", 
                            world_geo$name,
                            "<br><strong>Fully Vaccinated: </strong>", 
@@ -358,23 +422,25 @@ function(input, output, session) {
       leaflet(world_geo) %>%
         addTiles()  %>%
         setView( lat=10, lng=0 , zoom=2) %>%
-        addPolygons(
-          popup = partial_popup,
-          fillColor = ~mypalette(world_geo$percent_partial),
-          stroke=TRUE,
-          fillOpacity = 0.9,
-          color="white",
-          weight=0.3,
-          labelOptions = labelOptions(
-            style = list("font-weight" = "normal", padding = "3px 8px"),
-            textsize = "13px",
-            direction = "auto"
-          )
-        ) %>%
-        addLegend( pal=mypalette, values=~percent_partial, opacity=0.9, title = "Percent partially vaccinated by country", position = "bottomleft" )
-  })
-}
+        addPolygons(popup = partial_popup,
+                fillColor = ~mypalette(world_geo$percent_partial),
+                   stroke = TRUE,
+              fillOpacity = 0.9,
+                    color ="white",
+                   weight = 0.3,
+             labelOptions = labelOptions(style = list("font-weight" = "normal", 
+                                                            padding = "3px 8px"),
+                                      textsize = "13px",
+                                     direction = "auto")) %>%
+        addLegend( pal = mypalette, 
+                values =~ percent_partial, 
+               opacity = 0.9, 
+                 title = "Percent partially vaccinated by country", 
+              position = "bottomleft" )
+      
+                                                  })
+                               }
 
 
-#comment 
+#thanks for a great term!
 
--- ui.R
+++ ui.R
@@ -16,19 +16,15 @@ library(RColorBrewer)
 library(leaflet)
 
 dashboardPage(skin = "green",
-              
-              
   
-  dashboardHeader(
-    title = p("COVID-19 Global Vaccination", style = "color: aqua;"),
-    titleWidth = 450, 
+  dashboardHeader(title = p("COVID-19 Global Vaccination", 
+                            style = "color: aqua;"),
+             titleWidth = 450, 
   
-            dropdownMenu(type = "messages",
-               messageItem(
-                 from = "The Creators",
-                 message = "Emma, Katie, and Jack made this"))),
+             dropdownMenu(type = "messages",
+              messageItem(from = "The Creators",
+                       message = "Emma, Katie, and Jack made this"))),
   
-
   dashboardSidebar(
     
     a(href = "https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html"),  
@@ -36,34 +32,33 @@ dashboardPage(skin = "green",
     sidebarMenu(
       
       menuItem("Home", tabName = "Home", 
-               icon = icon("globe", lib = "glyphicon")),
+                          icon = icon("globe", lib = "glyphicon")),
       
       menuItem("Variants", tabName = "Variants", 
-               icon = icon("random", lib = "glyphicon")),
+                              icon = icon("random", lib = "glyphicon")),
       
       menuItem("Socioeconomics", tabName = "Socioeconomics", 
-               icon = icon("usd")),
+                                    icon = icon("usd")),
       
       menuItem("Population", tabName = "Population", 
-               icon = icon("user")),
+                                icon = icon("user")),
       
-      menuItem("CDC Vaccine Info", icon = icon("file-code-o"),
-                href = "https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html")
-    )
-  ),
+      menuItem("CDC Vaccine Info", 
+               icon = icon("file-code-o"),
+               href = "https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html"))
+               ),
   
   
   dashboardBody(
     
     tags$head(tags$style(HTML('.content-wrapper { overflow: auto; }'))),
     
-    tabItems(
-      
-      tabItem(tabName = "Home",
+       tabItems(
+         
+          tabItem(tabName = "Home",
               
              p(strong("Welcome to the Covid Vaccine Information Hub, please click
                       on the tabs to explore!"), align = "center"),
-             
              
              box(width = 12,
               p("The interactive globe below displays the relative number of vaccines 
@@ -71,34 +66,34 @@ dashboardPage(skin = "green",
               Use the interactive drop-down menu to explore different globes for the vaccine 
               manufacturers of interest. The spike location on the globe rests at the capital 
               of the country. 
-              Countries with no spikes indicate missing data, not zero vaccines distributed.", align = "center")),
+              Countries with no spikes indicate missing data, not zero vaccines distributed.",
+                align = "center")),
            
   
             selectInput(
                 "vaccine",
                 "Manufacturer:",
                 c("AstraZeneca" = "Oxford/AstraZeneca",
-                  "Moderna" = "Moderna",
-                  "Pfizer" = "Pfizer/BioNTech",
-                  "Johnson&Johnson" = "JJ")
-                
-              ),
+                      "Moderna" = "Moderna",
+                       "Pfizer" = "Pfizer/BioNTech",
+              "Johnson&Johnson" = "JJ")),
               
-                    box(width = 12, globeOutput("globe")
-                        )),
+                    box(width = 12, globeOutput("globe"))
+                       ),
       
-      tabItem(tabName = "Variants",
+          tabItem(tabName = "Variants",
               
                 box(width = 12, 
                 p("As the pandemic continues to evolve, different 
-                                   variants of COVID-19 have emerged. The graph above 
-                                   shows the timeline of when these specific variants emerged 
-                                   in the country of choice. Use the interactive drop-down menu in 
-                                   order to compare the differences inwhen variants emerged in 
-                                   countries over 2020 to 2021 timeline. 
-                                   As you explore, please note
-                                   the Delta variant of COVID-19 is the most recent prolific variant and 
-                                   the data is as of October 25th, 2021", align ="center")),
+                   variants of COVID-19 have emerged. The graph above 
+                   shows the timeline of when these specific variants emerged 
+                   in the country of choice. Use the interactive drop-down menu in 
+                   border to compare the differences inwhen variants emerged in 
+                   countries over 2020 to 2021 timeline. 
+                   As you explore, please note
+                   the Delta variant of COVID-19 is the most recent prolific variant and 
+                   the data is as of October 25th, 2021", 
+                  align ="center")),
                 
                 box(width = 12, plotOutput("globalvariants")),
                 
@@ -193,18 +188,18 @@ dashboardPage(skin = "green",
                      "United States",
                      "Uruguay",
                      "Zambia",
-                     "Zimbabwe")
-                  )
+                     "Zimbabwe"))
                 
-              ),
+                                     ),
       
       tabItem(tabName = "Socioeconomics",
               
               box(width = 12,
               p("The data maniputlated on this page is intended to illustrate
-                        disparities between vaccination rates across countries in different 
-                        socioeconmic standings. We hope these
-                        graphical displays accuratly display this global issue.", align = "center")),
+                disparities between vaccination rates across countries in different 
+                socioeconmic standings. We hope these
+                graphical displays accuratly display this global issue.",
+                align = "center")),
               
               box(width = 12, plotOutput("socioeconomicsbox")),
               
@@ -213,26 +208,25 @@ dashboardPage(skin = "green",
                     "Country",
                     "Country:",
                     c("Uganda" = "Uganda",
-                      "Togo" = "Togo",
-                      "Switzerland" = "Switzerland",
+                        "Togo" = "Togo",
+                 "Switzerland" = "Switzerland",
                       "Sweden" = "Sweden",
-                      "Singapore" = "Singapore",
-                      "Sierra Leone" = "Sierra Leone",
+                   "Singapore" = "Singapore",
+                "Sierra Leone" = "Sierra Leone",
                       "Rwanda" = "Rwanda",
-                      "Qatar" = "Qatar",
+                       "Qatar" = "Qatar",
                       "Norway" = "Norway",
-                      "Mozambique" = "Mozambique",
-                      "Mali" = "Mali",
+                  "Mozambique" = "Mozambique",
+                        "Mali" = "Mali",
                       "Malawi" = "Malawi",
-                      "Luxembourg" = "Luxembourg",
-                      "Ireland" = "Ireland",
-                      "Iceland" = "Iceland",
+                  "Luxembourg" = "Luxembourg",
+                     "Ireland" = "Ireland",
+                     "Iceland" = "Iceland",
                       "Gambia" = "Gambia",
-                      "Ethiopia" = "Ethiopia",
-                      "Denmark" = "Denmark",
-                      "Afghanistan" = "Afghanistan")
-                    
-                  )),
+                    "Ethiopia" = "Ethiopia",
+                     "Denmark" = "Denmark",
+                 "Afghanistan" = "Afghanistan"))
+              ),
               
               box(width = 12,
                   fluidRow(
@@ -242,7 +236,8 @@ dashboardPage(skin = "green",
                     valueBoxOutput("VaccinatedBox"),
                     
                     valueBoxOutput("Predominant_VaccineBox")
-                  ))),
+                  ))
+                                                       ),
       
       tabItem(tabName = "Population",
               
@@ -262,16 +257,17 @@ dashboardPage(skin = "green",
                     the bars for each country show the rate of fully vaccinated individuals 
                     per capita. The color of the bar indicates how much of the world population 
                     that individual country contributes. The population data for each country 
-                    comes from a 2020 population dataset.                       Creator Analysis: As the creators of this visualization, 
+                    comes from a 2020 population dataset. Creator Analysis: As the creators of this visualization, 
                     we found it extremely interesting that even though China and 
                     India equivalent and large portions of the world populations, 
                     they are at polar opposite sides of vaccine distributions per capita, 
                     with China recording the highest vaccine distributed per capita and India 
                     reporting the lowest. This leads to global questions such as accuracy in 
                     reporting data, access to life saving vaccines, and other barriers that may 
-                    distinguish these countries", align = "center")),
+                    distinguish these countries", 
+                    align = "center")),
               
-              box(width = 12, plotOutput("percapita"))
+                box(width = 12, plotOutput("percapita"))
               )
       
     ),
