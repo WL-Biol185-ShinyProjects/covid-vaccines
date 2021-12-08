667c032fde025f1b473520ffab460dadc3bd2d84
Katie Kern <kernk22@mail.wlu.edu>
2021-12-08 14:16:05 -0500

    cleaned up code part 1

--- server.R
+++ server.R
@@ -24,23 +24,27 @@ function(input, output, session) {
     worldcities_all_manu <- read_csv("CSVs/cat.csv")
     
     data(worldcities_all_manu, package = "maps") #injection scope, never ever do this again
+    
     x <- worldcities_all_manu %>% 
          filter(vaccine == input$vaccine) 
-    print(x)
+   
     nrows_x <- nrow(x)
-    cities <- x[order(x$vaccines_by_manu,decreasing=TRUE)[1:nrows_x],]
+    
+    cities <- x[order(x$vaccines_by_manu, decreasing=TRUE)[1:nrows_x],]
+    
     value  <- 1000 * cities$vaccines_by_manu / max(cities$vaccines_by_manu)
-    manu_globe <-globejs(bg="black", lat=cities$lat,     long=cities$long, value=value, 
-                           rotationlat=-0.34,     rotationlong=-0.38, fov=30)
+    
+    manu_globe <- globejs(bg = "black", lat = cities$lat, long = cities$long, value = value, 
+                           rotationlat = -0.34, rotationlong = -0.38, fov = 30)
   })
   
   
   output$percapita <- renderPlot({ 
     
     time_series_covid19_vaccine_global <- read.csv("CSVs/time_series_covid19_vaccine_global.csv")
+    
     hopkins <- time_series_covid19_vaccine_global
     
-    #left join hopkins with world cities in order to get population to do fully vaccinated per capita
     country_pop <- read.csv("CSVs/population_by_country_2020.csv") %>%
       mutate(Country..or.dependency. = as.character(Country..or.dependency.)) %>%
       mutate(pop = Population..2020.)
@@ -62,9 +66,6 @@ function(input, output, session) {
       mutate(World.Share = gsub("%", "", World.Share, fixed = TRUE)) %>%
       mutate(World.Share = as.numeric(World.Share))
     
-    
-    
-    
     ggplot(pop_hopkins_fully, aes(reorder(Country_Region, fully_per_capita),fully_per_capita, fill = World.Share)) +
       geom_bar(stat = "identity") +
       theme_classic() +
@@ -80,13 +81,11 @@ function(input, output, session) {
     
     variants <- read.csv("CSVs/covid-variants.csv")
     
-    
     variants_date <- variants %>%
       mutate(year = year(as_date(date))) %>%
       mutate(month = month(as_date(date))) %>%
       mutate(day = day(as_date(date))) %>%
       filter(variant == "Alpha" | variant == "Delta" | variant == "Gamma" | variant == "Iota" | variant == "Beta" | variant == "Eta" | variant == "Lambda")
-    
     
     variants_popular <- variants_date %>%
       filter(location == input$location) %>%
@@ -108,7 +107,6 @@ function(input, output, session) {
                                                                                                  ifelse(month == 11, "Nov",
                                                                                                         ifelse(month == 12, "Dec", NA))))))))))))) %>%
       mutate(month_write = factor(month_write, levels = c("Dec", "Nov", "Oct", "Sep", "Aug", "Jul", "Jun", "May", "Apr", "Mar", "Feb", "Jan")))
-    
     
     ggplot(variants_popular, aes(month_write, n, fill = variant)) +
       geom_bar(stat = "identity") +
